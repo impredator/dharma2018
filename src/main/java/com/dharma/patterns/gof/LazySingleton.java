dec0bfffffa5999a539a96ad863c3a8ca062d2a6
ashton <goashton@dharma.com>
2018-04-21 16:08:34 +0800

    Add singleton pattern

---
+++ src/main/java/com/dharma/patterns/gof/LazySingleton.java
@@ -0,0 +1,15 @@
+package com.dharma.patterns.gof;
+
+public class LazySingleton {
+    private static LazySingleton instance;
+
+    private LazySingleton() {
+    }
+
+    public static LazySingleton getInstance() {
+        if (instance == null) {
+            instance = new LazySingleton();
+        }
+        return instance;
+    }
+}
