60aee98ea9feefea842cdaebedd47f8ded944ef6
ashton <goashton@dharma.com>
2018-08-11 15:43:04 +0800

    refine lock

---
+++ src/main/java/com/dharma/concurrency/concurrent/locks/ReenterLock.java
@@ -0,0 +1,34 @@
+package com.dharma.concurrency.concurrent.locks;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReenterLock implements Runnable {
+    public static ReentrantLock lock = new ReentrantLock();
+    public static int i = 0;
+
+    @Override
+    public void run() {
+        for(int j = 0; j < 10000000; j++) {
+            lock.lock();
+            try {
+                i++;
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ReenterLock rl = new ReenterLock();
+        Thread t1 = new Thread(rl);
+        Thread t2 = new Thread(rl);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+        System.out.println(i);
+
+    }
+}
